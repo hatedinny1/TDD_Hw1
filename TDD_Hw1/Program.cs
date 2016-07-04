fc0d5fe86ae097397ec25bbd2a4c0a0d304936ef
powen <powen@3D87>
2016-07-04 23:15:45 +0800

    加入專案檔案。

---
+++ TDD_Hw1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_Hw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class GetSetSum
+    {
+
+        public IEnumerable<int> GetOrderSetSum(int setNum,Func<Order,int> orderType)
+        {
+            
+            var order = GetOrderList();
+            var count = 0;
+            while (count <= order.Count())
+            {
+                yield return order.Skip(count).Take(setNum).Sum(orderType);
+                count += setNum;
+            }
+        }
+
+        public IEnumerable<Order> GetOrderList()
+        {
+            var order = new List<Order>();
+            order.Add(new Order { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21 });
+            order.Add(new Order { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22 });
+            order.Add(new Order { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23 });
+            order.Add(new Order { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24 });
+            order.Add(new Order { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25 });
+            order.Add(new Order { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26 });
+            order.Add(new Order { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27 });
+            order.Add(new Order { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28 });
+            order.Add(new Order { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29 });
+            order.Add(new Order { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30 });
+            order.Add(new Order { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31 });
+            return order;
+        }
+    }
+}
---
+++ TDD_Hw1Tests/GetSetSumTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDD_Hw1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace TDD_Hw1.Tests
+{
+    [TestClass()]
+    public class GetSetSumTests
+    {
+        [TestMethod()]
+        public void SetNum_Is_3_SumCost_Is_6_15_24_21()
+        {
+            //arrange
+            var target = new GetSetSum();
+            //act
+            var actual = target.GetOrderSetSum(3, x => x.Cost);
+            //assert
+            var excepted = new List<int> { 6, 15, 24, 21 };
+            actual.Should().Equal(excepted);
+        }
+
+        [TestMethod]
+        public void SetNum_Is_4_SumRevenue_Is_50_66_60()
+        {
+            //arrange
+            var target = new GetSetSum();
+            //actual
+            var actual = target.GetOrderSetSum(4, x => x.Revenue);
+            //assert
+            var excepted = new List<int> { 50, 66, 60 };
+            actual.Should().Equal(excepted);
+        }
+    }
+}
